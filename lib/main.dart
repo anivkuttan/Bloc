404c3de10ee904566570a8841b5b1fccd1d6ecb4
anivkuttan <anivkutta196@gmail.com>
2023-04-16 19:11:28 +0530

    Bloc Oserver added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bloc_sample/bloc_observer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -6,6 +7,7 @@ import 'cubit/Counter_Cubit/counter_cubit_view.dart';
 import 'cubit/Counter_Cubit/theme_cubit_cubit.dart';
 
 void main() {
+  Bloc.observer = MyBlocObserver();
   runApp(const MyApp());
 }
 
